a3d0ddd8a3107f354054432e98d64b86db368d07
RonakFabian <ronakmanfabian@gmail.com>
2023-08-08 23:44:34 +0530

    Added libs and components

--- components/shared/Topbar.tsx
+++ components/shared/Topbar.tsx
@@ -1,13 +1,13 @@
 import { OrganizationSwitcher, SignedIn, SignOutButton } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
 import Image from "next/image";
 import Link from "next/link";
-import { dark } from "@clerk/themes";
 
 function Topbar() {
   return (
     <nav className="topbar">
       <Link href="/" className="flex items-center gap-4">
-        <Image src="assets/logo.svg" alt="logo" width={28} height={28} />
+        <Image src="/assets/logo.svg" alt="logo" width={28} height={28} />
         <p className="text-heading3-bold text-light-1 max-xs:hidden">Threads</p>
       </Link>
 
@@ -22,7 +22,6 @@ function Topbar() {
                   width={24}
                   height={24}
                 />
-                Threads
               </div>
             </SignOutButton>
           </SignedIn>
--- next.config.js
+++ next.config.js
@@ -4,11 +4,6 @@ const nextConfig = {
     serverActions: true,
     serverComponentsExternalPackages: ["mongoose"],
   },
-  eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: true,
-  },
   images: {
     remotePatterns: [
       {
@@ -28,6 +23,9 @@ const nextConfig = {
         hostname: "placehold.co",
       },
     ],
+    typescript: {
+      ignoreBuildErrors: true,
+    },
   },
 };
 
